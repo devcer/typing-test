da5ff8aa76cd436bc7c865f1fa5951b8d90885a4
devcer <viswanathamsantosh@gmail.com>
2020-11-22 18:41:38 +0530

    Added and configured countdown component and few other components in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { CountdownModule } from 'ngx-countdown';
 
 // Material module imports
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
-
-import { HomeComponent } from './pages/home/home.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -21,6 +26,10 @@ import { HomeComponent } from './pages/home/home.component';
     MatToolbarModule,
     MatTabsModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    CountdownModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CountdownComponent, CountdownEvent } from 'ngx-countdown';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   selectedTabIndex = 0;
-
-  constructor() { }
+  @ViewChild('countdown', { static: false })
+  private countdown: CountdownComponent;
+  config = {
+    leftTime: 60,
+  };
+  score = 0;
+  constructor() {}
 
   ngOnInit(): void {
     // this.selectedTabIndex = 0;
+    // this.countdown.begin();
   }
-  selectedTabChange(index: any): void {
+  selectedTabChange(index: any): void {}
+  handleEvent(ev: CountdownEvent): void {
+    // ev.action === done
+    // disable input box
+    // debugger;
+  }
+  onStartTyping(ev: Event ): void {
   }
 }
