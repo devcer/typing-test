029af15010535905831c0e0bd07f956a58d5acc2
devcer <viswanathamsantosh@gmail.com>
2020-11-22 20:37:03 +0530

    configured dummy text in assets, few more implementations

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CountdownComponent, CountdownEvent } from 'ngx-countdown';
+import {
+  CountdownComponent,
+  CountdownConfig,
+  CountdownEvent,
+} from 'ngx-countdown';
+import { paragraph } from 'src/assets/DummyData';
 
 @Component({
   selector: 'app-home',
@@ -8,24 +13,47 @@ import { CountdownComponent, CountdownEvent } from 'ngx-countdown';
 })
 export class HomeComponent implements OnInit {
   selectedTabIndex = 0;
+  timeSetList = [1, 3, 5];
   @ViewChild('countdown', { static: false })
   private countdown: CountdownComponent;
-  config = {
+  config: CountdownConfig = {
     leftTime: 60,
+    demand: true,
+    format: 'mm:ss',
   };
   score = 0;
+  stringArr: string[] = [];
+  showStartButton = true;
+  currentWordIndex = 0;
   constructor() {}
 
   ngOnInit(): void {
     // this.selectedTabIndex = 0;
     // this.countdown.begin();
+    paragraph.split(' ').forEach((text) => {
+      this.stringArr.push(`${text} `);
+    });
   }
-  selectedTabChange(index: any): void {}
+  selectedTabChange(index: any): void {
+    this.config.leftTime = this.timeSetList[index] * 60;
+    this.showStartButton = true;
+  }
   handleEvent(ev: CountdownEvent): void {
     // ev.action === done
     // disable input box
     // debugger;
   }
-  onStartTyping(ev: Event ): void {
+  onStartTyping(ev: Event): void {}
+  startTimer(): void {
+    this.countdown.begin();
+  }
+  startTypingTest(): void {
+    this.showStartButton = false;
+  }
+  onInputKeyUp(ev: KeyboardEvent): void {
+    const keycode = ev.code;
+    if (keycode === 'Space') {
+    } else {
+    }
   }
 }
